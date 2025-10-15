06d9f74c03a2e52cd6073b86f52031030ef5b3c9
Kartik Bhat <kbhat4203@gmail.com>
2025-10-15 23:02:32 +0530

    Updated WhatsApp button with new number and simpler greeting text

--- src/app/components/Whatsapp.tsx
+++ src/app/components/Whatsapp.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function WhatsAppButton() {
   return (
     <Link
-      href="https://wa.me/919876543210?text=Hi%20I%20would%20like%20to%20know%20more%20about%20your%20services"
+       href="https://wa.me/919011370514?text=Hi%20👋"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50"
